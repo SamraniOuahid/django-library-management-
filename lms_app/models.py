3f31012790ec5f608860123c92350e83d6d648c5
ouahidsamrani <ouahid.samrani@usmba.ac.ma>
2024-12-25 00:02:22 +0000

    models table book

--- lms_app/models.py
+++ lms_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+
+    status_book = [
+        ('available, available'),
+        ('rental','rental' ), 
+        ('sold', 'sold')
+    ]
+
+    title = models.CharField(max_length=250)
+    author = models.CharField(max_length=250, null=True, blank= True)
+    photo_book = models.ImageField(upload_to='photos', null=True, blank= True)
+    photo_author = models.ImageField(upload_to='photos', null=True, blank= True)
+    pages = models.IntegerField(null=True, blank= True)
+    price = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank= True)
+    rental_price_day = models.IntegerField(null=True, blank= True)
+    active = models.BooleanField(default=True)
+    status = models.CharField(max_length= 250, choices= status_book, null=True, blank= True)
+
+    
+
