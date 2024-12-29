b7af9e60188140dd87487b67a75ab72620c6c98c
ouahidsamrani <ouahid.samrani@usmba.ac.ma>
2024-12-29 15:03:56 +0000

    get of db

--- lms_app/views.py
+++ lms_app/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 import json
-from .models import Book
+from .models import *
 
 # Create your views here.
 def index(request):
-    return render(request, 'pages/index.html')
+    context = {
+        'book': Book.objects.all()
+    }
+    return render(request, 'pages/index.html', context)
 def books(request):
     return render(request, 'pages/books.html')
 
